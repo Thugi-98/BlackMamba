12d44644be65fd929911e2bb22bff236163d60d5
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2026-01-02 21:30:29 +0900

    26.01.02 문제풀이

---
+++ 이서연/백준 문제풀이/11659.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int size = scanner.nextInt();
+        int times = scanner.nextInt();
+
+        int[] sumArray = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            
+            int num = scanner.nextInt();
+
+            if (i == 0) {
+                sumArray[i] = num;
+                continue;
+            }
+
+            sumArray[i] = sumArray[i-1] + num;
+        }
+
+        for (int i = 0; i < times; i++) {
+
+            int start = scanner.nextInt();
+            int end = scanner.nextInt();
+
+            int answer = start == 1 ? sumArray[end-1] : sumArray[end-1] - sumArray[start-2];
+
+            System.out.println(answer);
+        }
+    }
+}
