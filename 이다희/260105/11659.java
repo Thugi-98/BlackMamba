809efff0f96cc17d5dd189eae039e2e011294f29
i-am-dahee <hxxly02@gmail.com>
2026-01-06 15:15:45 +0900

    feat: 26-01-05 문제풀이

---
+++ 이다희/260105/11659.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] list = new int[N];
+        int[] sum = new int[N + 1];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            list[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i <= N; i++) {
+            sum[i] = sum[i - 1] + list[i - 1];
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            System.out.println(sum[y] - sum[x - 1]);
+        }
+    }
+}
---
+++ 이다희/260105/1546.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] score = new int[N];
+
+        double M = 0;
+        double sum = 0;
+
+        for (int i = 0; i < N; i++) {
+            score[i] = sc.nextInt(); 
+            if (M < score[i]) {
+                M = score[i];
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            sum = sum + score[i] / M * 100;
+        }
+
+        System.out.print(sum / N);
+        
+        sc.close();
+    }
+}
