65ef229ca2155ffa5b138ade0b32abb9ffcaaa23
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2026-01-28 21:26:20 +0900

    260129 문제 풀이

---
+++ 이서연/260129 알고리즘 문제 풀이/N과_M_4.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class N과_M_4 {
+
+    private static int range, digit;
+    private static int[] numArr;
+    private static StringBuilder result = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        range = Integer.parseInt(st.nextToken());
+        digit = Integer.parseInt(st.nextToken());
+
+        numArr = new int[digit];
+
+        backtracking(0);
+
+        System.out.print(result);
+    }
+
+    private static void backtracking(int depth) {
+
+		// 배열 출력
+		if (depth == digit) {
+
+			for (int val : numArr) {
+				result.append(val).append(' ');
+			}
+			result.append('\n');
+			return;
+		}
+ 
+		for (int i = 0; i < range; i++) {
+
+            if (depth != 0 && numArr[depth-1] > i+1) {
+                continue;
+            }
+
+            numArr[depth] = i + 1;
+
+            // 재귀 호출
+            backtracking(depth + 1);
+		}
+	}
+}
